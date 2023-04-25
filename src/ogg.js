3c72cae6446696c8eef747f57398cb1ddc38f1c2
Evgeniy Salish <jeka5650962@gmail.com>
2023-04-25 20:08:43 +0300

    250423 2008

--- src/ogg.js
+++ src/ogg.js
@@ -1,9 +1,30 @@
+import axios from 'axios'
+import {createWriteStream} from 'fs'
+import {dirname, resolve} from 'path'
+import {fileURLToPath} from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 class OggConverter {
     constructor() {}
 
     toMp3() {}
 
-    create() {}
+    async create(url, filename) {
+        try {
+            const oggPath = resolve(__dirname, '../voices', `${filename}.ogg`)
+            const response = await axios({
+                method: 'get',
+                url,
+                responseType: 'stream'
+            })
+            const stream = createWriteStream(oggPath)
+            response.data.pipe(stream)
+            stream.on('finish', () => {})
+        } catch (e) {
+            console.log('Error while creating ogg', e.message)
+        }
+    }
 }
 
 export const ogg = new OggConverter()
